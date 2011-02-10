907d682994e0ae636b64e21483e139ad3b570f3c
geoffreyd <geoffreyd@gmail.com>
2011-02-11 08:24:30 +1000

    Clean install with a datasource

---
+++ apps/my_app/models/todo.js
@@ -0,0 +1,24 @@
+// ==========================================================================
+// Project:   MyApp.Todo
+// Copyright: ©2011 My Company, Inc.
+// ==========================================================================
+/*globals MyApp */
+
+/** @class
+
+  (Document your Model here)
+
+  @extends SC.Record
+  @version 0.1
+*/
+MyApp.Todo = SC.Record.extend(
+/** @scope MyApp.Todo.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
+
+MyApp.Todo.mixin({
+  resourcePath: 'todo',
+  pluralResourcePath: 'todos'
+}) ;
---
+++ apps/my_app/tests/models/todo.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   MyApp.Todo Unit Test
+// Copyright: ©2011 My Company, Inc.
+// ==========================================================================
+/*globals MyApp module test ok equals same stop start */
+
+module("MyApp.Todo");
+
+// TODO: Replace with real unit test for Todo
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
